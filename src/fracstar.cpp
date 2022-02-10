ee292454a8f2ab4b78781099b49a0ea9542b87ab
nsweb <nsweb@free.fr>
2022-02-10 08:45:32 +0100

    Rework fracstar application entry

--- src/fracstar.cpp
+++ src/fracstar.cpp
@@ -1,49 +1,65 @@
-// fracstar.cpp : Defines the entry point for the console application.
-//
 
 #include "fracstar.h"
-#include "system/profiler.h"
-#include "SDL_main.h"
+#include "common.h"
 
-//#define KSVD_IMPLEMENTATION
-//#define KSVD_NO_MATH
-//#define KSVD_NO_IOSTREAM
-//#define KSVD_USE_DOUBLE
-//#include "ksvd/trunk/ksvd.h"
-
-//int _tmain(int argc, _TCHAR* argv[])
-
-#ifdef __cplusplus
-extern "C"
-#endif
-int main(int argc, char *argv[])
+namespace
 {
-	PROFILE_THREAD_SCOPE("MainThread");
-
-	g_pEngine = new FSEngine();
-	CommandLine& CmdLine = g_pEngine->GetCommandLine();
-	CmdLine.Parse( argc, argv );
-    
-    EngineInitParams init_params;
-    init_params.resizable_window = false;
-    init_params.mouse_capture = true;
-    init_params.default_res_x = 800;
-    init_params.default_res_y = 600;
-	g_pEngine->Init( init_params );
-
-	String CmdType;
-	if( CmdLine.IsCommand( CmdType ) )
+class FractstarApp : public entry::AppI
+{
+public:
+	FractstarApp(const char* _name, const char* _description, const char* _url)
+		: entry::AppI(_name, _description, _url)
 	{
-		g_pEngine->RunCommand( CmdType, CmdLine.switches, CmdLine.tokens );
-	}
-	else
-	{
-		g_pEngine->MainLoop();
 	}
 
-	g_pEngine->Shutdown();
-	delete g_pEngine;
-	g_pEngine = nullptr;
+	void init(int32_t _argc, const char* const* _argv, uint32_t _width, uint32_t _height) override
+	{
+		FSEngine* engine = new FSEngine();
+		// (REBIND)
+		//CommandLine& CmdLine = g_pEngine->GetCommandLine();
+		//CmdLine.Parse(_argc, _argv);
 
-	return 0;
-}
+		EngineInitParams init_params;
+		init_params.resizable_window = false;
+		init_params.mouse_capture = true;
+		init_params.default_res_x = 1280;
+		init_params.default_res_y = 720;
+		init_params.argc = _argc;
+		init_params.argv = _argv;
+		init_params.app = this;
+		FSEngine::Get()->Init(init_params);
+
+		// (REBIND)
+		//String CmdType;
+		//if (CmdLine.IsCommand(CmdType))
+		//{
+		//	g_pEngine->RunCommand(CmdType, CmdLine.switches, CmdLine.tokens);
+		//}
+		//else
+		//{
+		//	g_pEngine->MainLoop();
+		//}
+	}
+
+	int shutdown() override
+	{
+		FSEngine::Get()->Shutdown();
+		delete FSEngine::Get();
+
+		return 0;
+	}
+
+	bool update() override
+	{
+		return FSEngine::Get()->MainLoop();
+	}
+};
+
+} // namespace
+
+ENTRY_IMPLEMENT_MAIN(
+	FractstarApp
+	, "_fracstar_"
+	, "Fracstar game"
+	, "https://github.com/nsweb/fracstar"
+	);
--- src/fracstar.h
+++ src/fracstar.h
@@ -1,18 +1,16 @@
-
 #pragma once
 
-#include "targetver.h"
+//#include "targetver.h"
 
-#include <stdio.h>
-#if !defined _MSC_VER
-
-#else
-#include <tchar.h>
-#endif
+//#include <stdio.h>
+//#if defined _MSC_VER
+//#include <tchar.h>
+//#endif
 
 #include "bigfx.h"
+#include <bx/math.h>
+#include "imgui.h"
 
 using namespace bigfx;
-
 
 #include "engine/fsengine.h"
